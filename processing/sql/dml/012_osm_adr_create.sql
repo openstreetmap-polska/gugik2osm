e7647087fd6f9b39be8b4a6944e340dca44ebde3
tomczk <tomasztaras@outlook.com>
2020-01-01 22:21:37 +0100

    added sql scripts for further steps in the process

---
+++ processing/sql/dml/012_osm_adr_create.sql
@@ -0,0 +1,33 @@
+drop table if exists osm_adr;
+create table osm_adr as
+with
+tab as (
+    select miejscowosc, cz_msc, ulica, type as nr_porzadkowy, kod_pna, kod_ulic, kod_simc, ST_Centroid(geometry) geom
+    from osm_addr_polygon
+    union all
+    select miejscowosc, cz_msc, ulica, type as nr_porzadkowy, kod_pna, kod_ulic, kod_simc, geometry
+    from osm_addr_point
+),
+a as (
+    select
+      case when miejscowosc = '' then null else trim(miejscowosc) end msc,
+      case when cz_msc = '' then null else trim(cz_msc) end czmsc,
+      case when ulica = '' then null else trim(ulica) end ul,
+      upper(trim(nr_porzadkowy)) nr,
+      case when kod_pna ~ '^\d{2}-\d{3}$' and kod_pna <> '00-000' then kod_pna else null end pna,
+      case when kod_ulic ~ '^\d{5}$' then kod_ulic else null end ulic,
+      case when kod_simc ~ '^\d{7}$' then kod_simc else null end simc,
+      geom
+    from tab
+)
+SELECT
+  a.*
+--   ,st_srid(geom) srid,
+--   st_x(geom) x,
+--   st_y(geom) y
+FROM a
+where
+  coalesce(msc, czmsc) is not null
+  and
+  nr is not null
+;
---
+++ processing/sql/dml/014_osm_hashed_create.sql
@@ -0,0 +1,9 @@
+drop table if exists osm_hashed;
+create table osm_hashed as
+    select md5(concat(lower(msc), coalesce(lower(ul), ''), nr)) hash, st_transform(geom, 2180) geom
+    from osm_adr
+    union all
+    select md5(concat(lower(czmsc), coalesce(lower(ul), ''), nr)) hash, st_transform(geom, 2180) geom
+    from osm_adr
+    where czmsc is not null
+;
