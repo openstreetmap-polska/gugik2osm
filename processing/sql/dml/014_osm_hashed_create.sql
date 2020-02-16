e542838f01ac902bf8964154a3d528477e6c3cd8
tomczk <tomasztaras@outlook.com>
2020-02-16 22:15:22 +0100

    fixed sql query syntax error

--- processing/sql/dml/014_osm_hashed_create.sql
+++ processing/sql/dml/014_osm_hashed_create.sql
@@ -1,5 +1,5 @@
 drop table if exists osm_hashed;
-create table unlogged osm_hashed as
+create unlogged table osm_hashed as
     select md5(concat(lower(msc), coalesce(lower(ul), ''), nr)) hash, st_transform(geom, 2180) geom
     from osm_adr
     union all
