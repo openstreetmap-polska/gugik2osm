c1255439792c4f8f76636319dd2839b7decea930
tomczk <tomasztaras@outlook.com>
2020-02-23 13:37:58 +0100

    sql optimizations for partial update process

--- processing/sql/partial_update/003_prg_delta_delete_bbox.sql
+++ processing/sql/partial_update/003_prg_delta_delete_bbox.sql
@@ -1,7 +1,19 @@
-delete from prg.delta
+delete from prg.delta d
+using prg.pa_hashed prg, osm_hashed osm
 where
   -- make sure given bounding box is valid
   ST_Transform(ST_MakeEnvelope(14.0, 49.0, 24.03, 54.86, 4326), 3857) && ST_MakeEnvelope(%(xmin)s, %(ymin)s, %(xmax)s, %(ymax)s, 3857)
   and
   geom && ST_Transform(ST_MakeEnvelope(%(xmin)s, %(ymin)s, %(xmax)s, %(ymax)s, 3857), 2180)
+  and
+  d.lokalnyid = prg.lokalnyid
+  and (
+    (
+      prg.hash = osm.hash
+      and
+      st_dwithin(prg.geom, osm.geom, 50)
+    )
+    or
+    st_dwithin(prg.geom, osm.geom, 5)
+  )
 ;
--- processing/sql/partial_update/006_lod1_delete.sql
+++ processing/sql/partial_update/006_lod1_delete.sql
@@ -1,7 +1,10 @@
 delete from prg.lod1_buildings b
+using osm_buildings o
 where
   -- make sure given bounding box is valid
   ST_Transform(ST_MakeEnvelope(14.0, 49.0, 24.03, 54.86, 4326), 3857) && ST_MakeEnvelope(%(xmin)s, %(ymin)s, %(xmax)s, %(ymax)s, 3857)
   and
   b.geom && ST_Transform(ST_MakeEnvelope(%(xmin)s, %(ymin)s, %(xmax)s, %(ymax)s, 3857), 4326)
+  and
+  st_intersects(b.geom, o.geometry)
 ;
