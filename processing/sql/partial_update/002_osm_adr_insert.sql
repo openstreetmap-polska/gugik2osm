d1d6163a32ad138ef0c627ad6ba843e89cd25463
tomczk <tomasztaras@outlook.com>
2020-02-27 23:13:02 +0100

    added missing sql queries to fix the partial update process
    these queries update one of the tables (osm_adr) that is used further in the process to update another table (osm_hashed)
    lack of these queries prevented address points from being updated

---
+++ processing/sql/partial_update/002_osm_adr_insert.sql
@@ -0,0 +1,40 @@
+insert into osm_adr
+with
+tab as (
+    select miejscowosc, cz_msc, ulica, type as nr_porzadkowy, kod_pna, kod_ulic, kod_simc, ST_Centroid(geometry) geom
+    from osm_addr_polygon
+    where
+      -- make sure given bounding box is valid
+      ST_Transform(ST_MakeEnvelope(14.0, 49.0, 24.03, 54.86, 4326), 3857) && ST_MakeEnvelope(%(xmin)s, %(ymin)s, %(xmax)s, %(ymax)s, 3857)
+      and
+      geometry && ST_Transform(ST_MakeEnvelope(%(xmin)s, %(ymin)s, %(xmax)s, %(ymax)s, 3857), 4326)
+      and
+      ST_Centroid(geometry) && ST_Transform(ST_MakeEnvelope(%(xmin)s, %(ymin)s, %(xmax)s, %(ymax)s, 3857), 4326)
+    union all
+    select miejscowosc, cz_msc, ulica, type as nr_porzadkowy, kod_pna, kod_ulic, kod_simc, geometry
+    from osm_addr_point
+    where
+      -- make sure given bounding box is valid
+      ST_Transform(ST_MakeEnvelope(14.0, 49.0, 24.03, 54.86, 4326), 3857) && ST_MakeEnvelope(%(xmin)s, %(ymin)s, %(xmax)s, %(ymax)s, 3857)
+      and
+      geometry && ST_Transform(ST_MakeEnvelope(%(xmin)s, %(ymin)s, %(xmax)s, %(ymax)s, 3857), 4326)
+),
+a as (
+    select
+      case when miejscowosc = '' then null else trim(miejscowosc) end msc,
+      case when cz_msc = '' then null else trim(cz_msc) end czmsc,
+      case when ulica = '' then null else trim(ulica) end ul,
+      upper(trim(nr_porzadkowy)) nr,
+      case when kod_pna ~ '^\d{2}-\d{3}$' and kod_pna <> '00-000' then kod_pna else null end pna,
+      case when kod_ulic ~ '^\d{5}$' then kod_ulic else null end ulic,
+      case when kod_simc ~ '^\d{7}$' then kod_simc else null end simc,
+      geom
+    from tab
+)
+SELECT a.*
+FROM a
+where
+  coalesce(msc, czmsc) is not null
+  and
+  nr is not null
+;
