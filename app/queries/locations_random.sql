f6bdcbc1536a59a54734545d576ddf5e6f14c57f
tomczk <tomasztaras@outlook.com>
2020-03-01 10:40:54 +0100

    removed predicate in one of SQL queries for random location

--- app/queries/locations_random.sql
+++ app/queries/locations_random.sql
@@ -1,4 +1,4 @@
 -- efficient sampling
 -- https://www.postgresql.org/docs/current/sql-select.html#SQL-FROM
 -- get around 1k rows from table that has around 100k rows (1%)
-SELECT st_x(geom) longitude, st_y(geom) latitude FROM prng TABLESAMPLE BERNOULLI ((1000 * 100) / 100000.0) where count > 10;
+SELECT st_x(geom) longitude, st_y(geom) latitude FROM prng TABLESAMPLE BERNOULLI ((1000 * 100) / 100000.0) ;
