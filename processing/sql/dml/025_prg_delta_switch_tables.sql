576ae51cdc97b6753610a747c0d9fc75385e814c
tomczk <tomasztaras@outlook.com>
2020-03-07 15:04:52 +0100

    fixed sql script that switches old and new prg.delta tables

--- processing/sql/dml/025_prg_delta_switch_tables.sql
+++ processing/sql/dml/025_prg_delta_switch_tables.sql
@@ -1,5 +1,5 @@
 alter table prg.delta rename to delta_old;
 alter table prg.delta_new rename to delta;
 drop table prg.delta_old;
-alter index delta_gis_new rename to delta_gis;
-alter index delta_lokalnyid_new rename to delta_lokalnyid;
+alter index prg.delta_gis_new rename to delta_gis;
+alter index prg.delta_lokalnyid_new rename to delta_lokalnyid;
