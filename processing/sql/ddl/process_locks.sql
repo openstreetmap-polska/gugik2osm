ba8666907977b1e1e2b9f48a464badcc8b5c6925
tomczk <tomasztaras@outlook.com>
2020-03-14 18:51:06 +0100

    alter process_locks table to allow tracking time when process started and ended

--- processing/sql/ddl/process_locks.sql
+++ processing/sql/ddl/process_locks.sql
@@ -3,4 +3,8 @@ create table if not exists process_locks (
   in_progress boolean not null
 );
 
-insert into process_locks values ('prg_full_update', false) on conflict do nothing;
+alter table process_locks add column if not exists start_time timestamp with time zone;
+alter table process_locks add column if not exists end_time timestamp with time zone;
+
+insert into process_locks values ('prg_full_update', false, null, null) on conflict do nothing;
+insert into process_locks values ('teryt_update', false, null, null) on conflict do nothing;
