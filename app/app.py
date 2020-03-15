b4b084acd6f13ba5cc4dbe3f2a9ef43b73979194
tomczk <tomasztaras@outlook.com>
2020-03-15 17:37:56 +0100

    refactored endpoints returning address and building data by creating additional functions that prepare the data instead of having everything in big methods

--- app/app.py
+++ app/app.py
@@ -56,12 +56,73 @@ def execute_sql(cursor, query: str, parameters: Union[tuple, dict] = None):
 
 
 def to_merc(bbox: m.LngLatBbox) -> dict:
+    """Method reprojects BBox in WGS84 (latitude, longitude) to Web Mercator."""
     in_proj = Proj('epsg:4326')
     out_proj = Proj('epsg:3857')
     res = dict()
     res["west"], res["south"] = transform(in_proj, out_proj, bbox.south, bbox.west)
     res["east"], res["north"] = transform(in_proj, out_proj, bbox.north, bbox.east)
     return res
+
+
+def addresses_xml(list_of_tuples):
+    """Method creates XML with address points in OSM schema."""
+    root = etree.Element('osm', version='0.6')
+    i = -1  # counter for fake ids
+    for t in list_of_tuples:
+        el = etree.Element('node', id=str(i), lat=str(t[8]), lon=str(t[7]))
+        el.append(deepcopy(SOURCE_ADDR))
+        el.append(etree.Element('tag', k='ref:addr', v=t[0]))
+        el.append(etree.Element('tag', k='addr:city:simc', v=t[2]))
+        if t[3]:
+            el.append(etree.Element('tag', k='addr:city', v=t[1]))
+            el.append(etree.Element('tag', k='addr:street', v=t[3]))
+            el.append(etree.Element('tag', k='addr:street:sym_ul', v=t[4]))
+        else:
+            el.append(etree.Element('tag', k='addr:place', v=t[1]))
+        el.append(etree.Element('tag', k='addr:housenumber', v=t[5]))
+        if t[6]:
+            el.append(etree.Element('tag', k='addr:postcode', v=t[6]))
+        root.append(el)
+        i -= 1
+    return root
+
+
+def buildings_xml(list_of_tuples):
+    """Method creates XML with buildings (polygons) in OSM schema."""
+    root = etree.Element('osm', version='0.6')
+    i = -1  # counter for fake ids
+    n = {}  # list of nodes
+    lst = []  # list of ways
+    # cursor returns tuple of (way_id, array_of_points[])
+    for t in list_of_tuples:
+        # create 'way' node for xml tree
+        way = etree.Element('way', id=str(t[0]))
+        way.append(deepcopy(BUILDING_TAG))
+        way.append(deepcopy(SOURCE_BUILDING))
+
+        # iterate over array of points that make the polygon and add references to them to the way xml node
+        for xy in t[1]:
+            # if given point is already in our list of nodes then:
+            if n.get(tuple(xy)):
+                way.append(deepcopy(n[tuple(xy)]['el']))
+                # appending doesn't work when you try to pass the same object
+                # you need to create new object if you want nodes with duplicate values
+                # since polygons start and end with the same node we need to deepcopy the object
+            else:
+                temp = etree.Element('nd', ref=str(i))
+                way.append(temp)
+                n[tuple(xy)] = {'el': temp, 'id': i}
+            i -= 1
+        lst.append(way)
+
+    for k, v in n.items():
+        root.append(etree.Element('node', id=str(v['id']), lat=str(k[1]), lon=str(k[0])))
+
+    for w in lst:
+        root.append(w)
+
+    return root
 
 
 @app.route('/prg/not_in/osm/', methods=['GET'])
@@ -84,26 +145,7 @@ def features_in():
     )
 
     if request.args.get('format') == 'osm':
-        root = etree.Element('osm', version='0.6')
-        i = -1  # counter for fake ids
-        for t in cur:
-            el = etree.Element('node', id=str(i), lat=str(t[8]), lon=str(t[7]))
-            el.append(deepcopy(SOURCE_ADDR))
-            el.append(etree.Element('tag', k='ref:addr', v=t[0]))
-            el.append(etree.Element('tag', k='addr:city:simc', v=t[2]))
-            if t[3]:
-                el.append(etree.Element('tag', k='addr:city', v=t[1]))
-                el.append(etree.Element('tag', k='addr:street', v=t[3]))
-                el.append(etree.Element('tag', k='addr:street:sym_ul', v=t[4]))
-            else:
-                el.append(etree.Element('tag', k='addr:place', v=t[1]))
-            el.append(etree.Element('tag', k='addr:housenumber', v=t[5]))
-            if t[6]:
-                el.append(etree.Element('tag', k='addr:postcode', v=t[6]))
-            root.append(el)
-            i -= 1
-
-        # etree.ElementTree(root).write(fpath, encoding='UTF-8')
+        root = addresses_xml(cur.fetchall())
         cur.close()
         return Response(
             etree.tostring(root, encoding='UTF-8'),
@@ -131,8 +173,8 @@ notes = {
     'informacja': '''Modele 3D budynków nie stanowią rejestru publicznego ani elementu treści takiego rejestru. W konsekwencji czego mają wartość jedynie poglądową. Niezgodność Modeli 3D budynków ze stanem faktycznym lub prawnym, tak w postaci nieprzetworzonej jak i po ich ewentualnym przetworzeniu w procesie ponownego wykorzystania, nie może stanowić podstawy odpowiedzialności Głównego Geodety Kraju z jakiegokolwiek tytułu wobec jakiegokolwiek podmiotu.''',
     'licencja': r'https://integracja.gugik.gov.pl/Budynki3D/GUGiK_Licencja_na_Budynki3D.pdf'
 }
-bld = etree.Element('tag', k='building', v='yes')
-src = etree.Element('tag', k='source', v='www.geoportal.gov.pl')
+BUILDING_TAG = etree.Element('tag', k='building', v='yes')
+SOURCE_BUILDING = etree.Element('tag', k='source', v='www.geoportal.gov.pl')
 SOURCE_ADDR = etree.Element('tag', k='source:addr', v='gugik.gov.pl')
 
 
@@ -161,38 +203,7 @@ def buildings():
     )
 
     if request.args.get('format') == 'osm':
-        root = etree.Element('osm', version='0.6')
-        i = -1  # counter for fake ids
-        n = {}  # list of nodes
-        lst = []  # list of ways
-        # cursor returns tuple of (way_id, array_of_points[])
-        for t in cur:
-            # create 'way' node for xml tree
-            way = etree.Element('way', id=str(t[0]))
-            way.append(deepcopy(bld))
-            way.append(deepcopy(src))
-
-            # iterate over array of points that make the polygon and add references to them to the way xml node
-            for xy in t[1]:
-                # if given point is already in our list of nodes then:
-                if n.get(tuple(xy)):
-                    way.append(deepcopy(n[tuple(xy)]['el']))
-                    # appending doesn't work when you try to pass the same object
-                    # you need to create new object if you want nodes with duplicate values
-                    # since polygons start and end with the same node we need to deepcopy the object
-                else:
-                    temp = etree.Element('nd', ref=str(i))
-                    way.append(temp)
-                    n[tuple(xy)] = {'el': temp, 'id': i}
-                i -= 1
-            lst.append(way)
-
-        for k, v in n.items():
-            root.append(etree.Element('node', id=str(v['id']), lat=str(k[1]), lon=str(k[0])))
-
-        for w in lst:
-            root.append(w)
-
+        root = buildings_xml(cur.fetchall())
         cur.close()
         return Response(
             etree.tostring(root, encoding='UTF-8'),
