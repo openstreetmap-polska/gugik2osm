f37ed27ba670ee86c33600ee533d2446f05b64bd
tomczk <tomasztaras@outlook.com>
2020-03-20 20:26:36 +0100

    add index on simc to prg.delta table

--- processing/sql/dml/024_prg_delta_set_logged.sql
+++ processing/sql/dml/024_prg_delta_set_logged.sql
@@ -1,1 +1,2 @@
+create index delta_simc_new on prg.delta_new using hash (teryt_simc);
 alter table prg.delta_new set logged;
--- processing/sql/dml/025_prg_delta_switch_tables.sql
+++ processing/sql/dml/025_prg_delta_switch_tables.sql
@@ -3,4 +3,5 @@ alter table prg.delta_new rename to delta;
 drop table prg.delta_old;
 alter index prg.delta_gis_new rename to delta_gis;
 alter index prg.delta_lokalnyid_new rename to delta_lokalnyid;
+alter index prg.delta_simc_new rename to delta_simc;
 vacuum analyze prg.delta;
