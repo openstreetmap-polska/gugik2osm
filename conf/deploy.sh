2baa85dd02c8dd3cbb98ca8f198209db8415c637
tomczk <tomasztaras@outlook.com>
2020-03-20 19:56:27 +0100

    changed deploy script

--- conf/deploy.sh
+++ conf/deploy.sh
@@ -11,7 +11,7 @@ cp -r /opt/gugik2osm/git/web/* /opt/gugik2osm/web/
 cp /opt/gugik2osm/git/conf/*.sh /opt/gugik2osm/conf/
 chmod 775 /opt/gugik2osm/conf/*.sh
 sudo cp /opt/gugik2osm/git/conf/cronfile /opt/gugik2osm/conf/
-cp /opt/gugik2osm/git/conf/*.conf /opt/gugik2osm/conf/
+cp /opt/gugik2osm/git/conf/nginx.conf /opt/gugik2osm/conf/
 
 cp /opt/gugik2osm/git/imposm3/mapping.yaml /opt/gugik2osm/imposm3/
 cp /opt/gugik2osm/git/imposm3/poland.geojson /opt/gugik2osm/imposm3/
