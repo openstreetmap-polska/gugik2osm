8c1a26fb828bb84e9e679ab8aaee5e8cffb4db23
tomczk <tomasztaras@outlook.com>
2020-03-22 22:19:58 +0100

    changed how files with address data is exported

--- conf/export_prg_addresses.sh
+++ conf/export_prg_addresses.sh
@@ -1,2 +1,3 @@
-psql -d gugik2osm -f /opt/gugik2osm/git/processing/sql/export/prg_addresses.sql | zip -9 /opt/gugik2osm/temp/export/prg_addresses.zip -
-cp /opt/gugik2osm/temp/export/prg_addresses.zip /var/www/data/prg_addresses.zip
+psql -d gugik2osm -f /opt/gugik2osm/git/processing/sql/export/prg_addresses.sql > /opt/gugik2osm/temp/export/prg_addresses.csv
+zip -9 /opt/gugik2osm/temp/export/prg_addresses.zip /opt/gugik2osm/temp/export/prg_addresses.csv
+mv /opt/gugik2osm/temp/export/prg_addresses.zip /var/www/data/prg_addresses.zip
