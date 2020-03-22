45264b949b6a0be4ce66a29db11e834f6dd7c9e8
tomczk <tomasztaras@outlook.com>
2020-03-22 09:56:21 +0100

    changed how prng table is created

--- processing/sql/dml/028_prng_create.sql
+++ processing/sql/dml/028_prng_create.sql
@@ -1,5 +1,4 @@
-drop table if exists prng;
-create unlogged table prng AS
+create unlogged table prng_new AS
 SELECT
     prng.id,
     prng.geom,
@@ -9,8 +8,16 @@ CROSS JOIN lateral (select count(*) liczba_adresow from prg.delta a where st_dwi
 CROSS JOIN lateral (select count(*) liczba_budynkow from prg.lod1_buildings b where st_dwithin(prng.geom, b.geom, 0.01)) bud
 ;
 
-create index idx_prng_geom on prng using gist (geom);
-cluster prng using idx_prng_geom;
-create index idx_prng_count on prng using btree (count desc);
+create index idx_prng_geom_new on prng using gist (geom);
+cluster prng using idx_prng_geom_new;
+create index idx_prng_count_new on prng using btree (count desc);
 --create index idx_prng_id on prng using btree (id);
-alter table prng set logged;
+alter table prng_new set logged;
+
+alter table prng rename to prng_old;
+alter table prng_new rename to prng;
+drop table prng_old;
+alter index idx_prng_geom_new rename to idx_prng_geom;
+alter index idx_prng_count_new rename to idx_prng_count;
+
+analyze prng;
