ed1a4aadd8dda8d722b7013572cb5dd48180a2f5
tomczk <tomasztaras@outlook.com>
2020-03-29 23:33:34 +0200

    removed unnecessary fields from export of osm buildings

--- conf/export_osm_buildings.sh
+++ conf/export_osm_buildings.sh
@@ -1,4 +1,4 @@
-ogr2ogr -f "GPKG" /opt/gugik2osm/temp/export/osm_buildings.gpkg PG:"dbname='gugik2osm" "osm_buildings" -nln "budynki" -gt 65536 -overwrite
+ogr2ogr -f "GPKG" /opt/gugik2osm/temp/export/osm_buildings.gpkg PG:"dbname='gugik2osm" "osm_buildings" -nln "budynki" -gt 65536 -select "geometry" -overwrite
 zip -9 -j /opt/gugik2osm/temp/export/osm_buildings.zip /opt/gugik2osm/temp/export/osm_buildings.gpkg
 mv /opt/gugik2osm/temp/export/osm_buildings.zip /var/www/data/osm_buildings.zip
 rm /opt/gugik2osm/temp/export/osm_buildings.gpkg
