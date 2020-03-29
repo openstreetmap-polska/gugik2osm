c1a67a7bb8ab294b2ca023e0be494fd2728d5ee0
tomczk <tomasztaras@outlook.com>
2020-03-29 22:44:22 +0200

    changed some configurations for scripts exporting data

--- conf/export_osm_addresses.sh
+++ conf/export_osm_addresses.sh
@@ -1,3 +1,4 @@
 psql -d gugik2osm -f /opt/gugik2osm/git/processing/sql/export/osm_addresses.sql > /opt/gugik2osm/temp/export/osm_addresses.csv
-zip -9 /opt/gugik2osm/temp/export/osm_addresses.zip /opt/gugik2osm/temp/export/osm_addresses.csv
+zip -9 -j /opt/gugik2osm/temp/export/osm_addresses.zip /opt/gugik2osm/temp/export/osm_addresses.csv
 mv /opt/gugik2osm/temp/export/osm_addresses.zip /var/www/data/osm_addresses.zip
+rm /opt/gugik2osm/temp/export/osm_addresses.csv
--- conf/export_prg_addresses.sh
+++ conf/export_prg_addresses.sh
@@ -1,3 +1,4 @@
 psql -d gugik2osm -f /opt/gugik2osm/git/processing/sql/export/prg_addresses.sql > /opt/gugik2osm/temp/export/prg_addresses.csv
-zip -9 /opt/gugik2osm/temp/export/prg_addresses.zip /opt/gugik2osm/temp/export/prg_addresses.csv
+zip -9 -j /opt/gugik2osm/temp/export/prg_addresses.zip /opt/gugik2osm/temp/export/prg_addresses.csv
 mv /opt/gugik2osm/temp/export/prg_addresses.zip /var/www/data/prg_addresses.zip
+rm /opt/gugik2osm/temp/export/prg_addresses.csv
