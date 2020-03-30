9565327da826b75f9908e68b954300a41cac3276
tomczk <tomasztaras@outlook.com>
2020-03-30 21:50:54 +0200

    added new unified endpoint to download data about addresses and buildings

--- app/app.py
+++ app/app.py
@@ -67,6 +67,60 @@ def to_merc(bbox: m.LngLatBbox) -> dict:
     return res
 
 
+def addresses_nodes(list_of_tuples: list) -> etree.Element:
+    i = -1  # counter for fake ids
+    for t in list_of_tuples:
+        el = etree.Element('node', id=str(i), lat=str(t[8]), lon=str(t[7]))
+        el.append(deepcopy(SOURCE_ADDR))
+        # do not add 'ref:addr' tag
+        # el.append(etree.Element('tag', k='ref:addr', v=t[0]))
+        el.append(etree.Element('tag', k='addr:city:simc', v=t[2]))
+        if t[3]:
+            el.append(etree.Element('tag', k='addr:city', v=t[1]))
+            el.append(etree.Element('tag', k='addr:street', v=t[3]))
+            el.append(etree.Element('tag', k='addr:street:sym_ul', v=t[4]))
+        else:
+            el.append(etree.Element('tag', k='addr:place', v=t[1]))
+        el.append(etree.Element('tag', k='addr:housenumber', v=t[5]))
+        if t[6]:
+            el.append(etree.Element('tag', k='addr:postcode', v=t[6]))
+        i -= 1
+        yield el
+
+
+def buildings_nodes(list_of_tuples: list) -> etree.Element:
+    i = -100000  # counter for fake ids
+    n = {}  # list of nodes
+    lst = []  # list of ways
+    # cursor returns tuple of (way_id, array_of_points[])
+    for t in list_of_tuples:
+        # create 'way' node for xml tree
+        way = etree.Element('way', id=str(t[0]))
+        way.append(deepcopy(BUILDING_TAG))
+        way.append(deepcopy(SOURCE_BUILDING))
+
+        # iterate over array of points that make the polygon and add references to them to the way xml node
+        for xy in t[1]:
+            # if given point is already in our list of nodes then:
+            if n.get(tuple(xy)):
+                way.append(deepcopy(n[tuple(xy)]['el']))
+                # appending doesn't work when you try to pass the same object
+                # you need to create new object if you want nodes with duplicate values
+                # since polygons start and end with the same node we need to deepcopy the object
+            else:
+                temp = etree.Element('nd', ref=str(i))
+                way.append(temp)
+                n[tuple(xy)] = {'el': temp, 'id': i}
+            i -= 1
+        lst.append(way)
+
+    for k, v in n.items():
+        yield etree.Element('node', id=str(v['id']), lat=str(k[1]), lon=str(k[0]))
+
+    for w in lst:
+        yield w
+
+
 def addresses_xml(list_of_tuples):
     """Method creates XML with address points in OSM schema."""
     root = etree.Element('osm', version='0.6')
@@ -126,6 +180,50 @@ def buildings_xml(list_of_tuples):
         root.append(w)
 
     return root
+
+
+@app.route('/josm_data', methods=['GET'])
+def download_josm_data():
+    """Newer version of the function returning data as osm file with the new endpoint."""
+
+    if request.args.get('filter_by') == 'bbox':
+        if not (
+                'xmin' in request.args and 'xmax' in request.args
+                and
+                'ymin' in request.args and 'ymax' in request.args
+        ):
+            abort(400)
+
+    root = etree.Element('osm', version='0.6')
+
+    cur = execute_sql(
+        pgdb().cursor(),
+        QUERIES['delta_where_bbox'],
+        (float(request.args.get('xmin')),
+         float(request.args.get('ymin')),
+         float(request.args.get('xmax')),
+         float(request.args.get('ymax')))
+    )
+
+    for an in addresses_nodes(cur.fetchall()):
+        root.append(an)
+
+    cur = execute_sql(
+        cur,
+        QUERIES['buildings_vertices'],
+        (float(request.args.get('xmin')), float(request.args.get('ymin')),
+         float(request.args.get('xmax')), float(request.args.get('ymax')))
+    )
+
+    for bn in buildings_nodes(cur.fetchall()):
+        root.append(bn)
+
+    cur.close()
+
+    return Response(
+        etree.tostring(root, encoding='UTF-8'),
+        mimetype='text/xml',
+        headers={'Content-disposition': 'attachment; filename=paczka_danych.osm'})
 
 
 @app.route('/prg/not_in/osm/', methods=['GET'])
