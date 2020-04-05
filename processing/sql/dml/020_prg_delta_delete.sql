80c7759c38fa19ec2a23278c8afc4ca538314f35
tomczk <tomasztaras@outlook.com>
2020-04-05 13:33:24 +0200

    fixes to SQL queries removing records from prg.delta table during full update process

--- processing/sql/dml/020_prg_delta_delete.sql
+++ processing/sql/dml/020_prg_delta_delete.sql
@@ -2,7 +2,7 @@ delete from prg.delta_new
 where lokalnyid in (
     select delta_new.lokalnyid
     from prg.delta_new
-    join prg.punkty_adresowe on delta.lokalnyid = punkty_adresowe.lokalnyid::uuid
+    join prg.punkty_adresowe on delta_new.lokalnyid = punkty_adresowe.lokalnyid::uuid
     where (punkty_adresowe.waznydo is not null and punkty_adresowe.waznydo::timestamptz < now()) or punkty_adresowe.koniecwersjiobiektu is not null
 );
 analyze prg.delta_new;
