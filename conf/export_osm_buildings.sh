0de6ba9515f7d8eba90b4871daf48e9cdd0fe639
tomczk <tomasztaras@outlook.com>
2020-04-19 18:31:54 +0200

    added fields to osm buildings export script

--- conf/export_osm_buildings.sh
+++ conf/export_osm_buildings.sh
@@ -1,4 +1,4 @@
-ogr2ogr -f "GPKG" /opt/gugik2osm/temp/export/osm_buildings.gpkg PG:"dbname=gugik2osm" "osm_buildings" -nln "budynki" -gt 65536 -select "geometry" -overwrite
+ogr2ogr -f "GPKG" /opt/gugik2osm/temp/export/osm_buildings.gpkg PG:"dbname=gugik2osm" "osm_buildings" -nln "budynki" -gt 65536 -select "osm_id,budynek,kondygnacje,ksztalt_dachu,liczba_mieszkan,kondygnacje_podziemne,wysokosc_npg,geometry" -overwrite
 zip -9 -j /opt/gugik2osm/temp/export/osm_buildings.zip /opt/gugik2osm/temp/export/osm_buildings.gpkg
 mv /opt/gugik2osm/temp/export/osm_buildings.zip /var/www/data/osm_buildings.zip
 rm /opt/gugik2osm/temp/export/osm_buildings.gpkg
