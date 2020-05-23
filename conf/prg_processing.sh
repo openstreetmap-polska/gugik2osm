c3aba9ffc4919e47b3d4f9bd30eef61b2cf97d67
tomczk <tomasztaras@outlook.com>
2020-05-23 13:06:59 +0200

    Changed prg_processing scripts to start from the first dml file (skip ddl files preparing schema).
    This should decrease chance of lock conflicts and any schema change will be applied manually anyway first to test if it's working as intended.

--- conf/prg_processing.sh
+++ conf/prg_processing.sh
@@ -21,6 +21,6 @@ python3.7 -u /opt/gugik2osm/git/processing/parsers/prg.py --input /opt/gugik2osm
 python3.7 -u /opt/gugik2osm/git/processing/parsers/prg.py --input /opt/gugik2osm/temp/30_Punkty_Adresowe.zip --writer postgresql --dsn "host=$PGHOSTADDR port=$PGPORT dbname=$PGDATABASE user=$PGUSER password=$PGPASSWORD" >> /opt/gugik2osm/log/prg_processing.log 2>&1
 python3.7 -u /opt/gugik2osm/git/processing/parsers/prg.py --input /opt/gugik2osm/temp/32_Punkty_Adresowe.zip --writer postgresql --dsn "host=$PGHOSTADDR port=$PGPORT dbname=$PGDATABASE user=$PGUSER password=$PGPASSWORD" >> /opt/gugik2osm/log/prg_processing.log 2>&1
 date >> /opt/gugik2osm/log/prg_processing.log
-python3.7 -u /opt/gugik2osm/git/processing/scripts/prg_prepare.py --full --dsn "host=$PGHOSTADDR port=$PGPORT dbname=$PGDATABASE user=$PGUSER password=$PGPASSWORD" >> /opt/gugik2osm/log/prg_processing.log 2>&1
+python3.7 -u /opt/gugik2osm/git/processing/scripts/prg_prepare.py --full --dsn "host=$PGHOSTADDR port=$PGPORT dbname=$PGDATABASE user=$PGUSER password=$PGPASSWORD" --starting 001_prg_pa_insert.sql >> /opt/gugik2osm/log/prg_processing.log 2>&1
 date >> /opt/gugik2osm/log/prg_processing.log
 
