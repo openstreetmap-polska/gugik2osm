453b1033896b6f8f141b04e5287c74b596e4e49e
tomczk <tomasztaras@outlook.com>
2020-11-23 22:13:27 +0100

    #36 - changed some conditions comparing PRG and OSM addresses as per public request

--- processing/sql/dml/022_prg_delta_delete.sql
+++ processing/sql/dml/022_prg_delta_delete.sql
@@ -3,7 +3,7 @@ using (
     SELECT prg.lokalnyid
     FROM prg.delta_new prg
     JOIN osm_addr_polygon osm
-        ON (st_intersects(prg.geom, st_transform(osm.geometry, 2180)) AND (osm.name IS NOT NULL)) -- name = nr
+        ON (st_intersects(prg.geom, st_transform(osm.geometry, 2180)) AND (osm.name = prg.nr)) -- name = nr
 ) dd
 where delta_new.lokalnyid = dd.lokalnyid;
 analyze prg.delta_new;
