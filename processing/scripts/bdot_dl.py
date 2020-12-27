3b4e93c317b62d5cd087a250b46c55e4ec0a685a
tomczk <tomasztaras@outlook.com>
2020-12-27 13:41:38 +0100

    added error handling for bdot10k files downloader

--- processing/scripts/bdot_dl.py
+++ processing/scripts/bdot_dl.py
@@ -4,21 +4,41 @@ import urllib.request
 import urllib.error
 import shutil
 from os import path
-
+from time import sleep
+from typing import Tuple
 
 BASE_URL = 'https://opendata.geoportal.gov.pl/bdot10k/{woj}/{pow}_GML.zip'
 
 
-def download_file(url: str, file_path: str):
-    print(datetime.now(timezone.utc).astimezone().isoformat(), '- downloading:', fn, 'from:', url)
+def download_file(url: str, file_path: str, _try_number: int = 1) -> None:
+    print(datetime.now(timezone.utc).astimezone().isoformat(), '- downloading:', file_path, 'from:', url)
     try:
         with urllib.request.urlopen(url) as response:
             with open(file_path, 'wb') as out_file:
                 shutil.copyfileobj(response, out_file)
     except urllib.error.HTTPError as e:
-        print(datetime.now(timezone.utc).astimezone().isoformat(), '- There was a problem downloading:', fn,
-              'from:', url,
-              '- Status code:', e.code, '-', e)
+        if _try_number > 3:
+            print(datetime.now(timezone.utc).astimezone().isoformat(), '- There was a problem downloading:', file_path,
+                  'from:', url,
+                  '- Status code:', e.code, '-', e)
+            raise e
+        else:
+            sleep(2)
+            download_file(url, file_path, _try_number=_try_number + 1)
+    except ConnectionResetError as e2:
+        if _try_number > 3:
+            raise e2
+        else:
+            sleep(2)
+            download_file(url, file_path, _try_number=_try_number+1)
+
+
+def prepare_url_and_filepath(teryt_code: str) -> Tuple[str, str]:
+    teryt_code = args.get('only')
+    fn = 'BDOT10k_{0}.zip'.format(teryt_code)
+    url = BASE_URL.format(woj=teryt_code[:2], pow=teryt_code)
+    file_path = path.join(args['output_dir'][0], fn)
+    return url, file_path
 
 
 if __name__ == '__main__':
@@ -29,10 +49,7 @@ if __name__ == '__main__':
     args = vars(parser.parse_args())
 
     if args.get('only'):
-        teryt_code = args.get('only')
-        fn = 'BDOT10k_{0}.zip'.format(teryt_code)
-        url = BASE_URL.format(woj=teryt_code[:2], pow=teryt_code)
-        file_path = path.join(args['output_dir'][0], fn)
+        url, file_path = prepare_url_and_filepath(args.get('only'))
         download_file(url, file_path)
     else:
         import psycopg2 as pg
@@ -47,8 +64,5 @@ if __name__ == '__main__':
                 print(datetime.now(timezone.utc).astimezone().isoformat(), '- There was a problem executing query.', e)
 
             for tup in cur.fetchall():
-                teryt_code = tup[0]
-                fn = 'BDOT10k_{0}.zip'.format(teryt_code)
-                url = BASE_URL.format(woj=teryt_code[:2], pow=teryt_code)
-                file_path = path.join(args['output_dir'][0], fn)
+                url, file_path = prepare_url_and_filepath(tup[0])
                 download_file(url, file_path)
