e6b185cf705395816aea8fed7907c036acd9fdf9
tomczk <tomasztaras@outlook.com>
2020-12-30 19:15:37 +0100

    #47 - changed limit of distance between PRG address and OSM address for query removing duplicated points from 5 meters to 2 meters

--- processing/sql/dml/018_prg_delta_delete.sql
+++ processing/sql/dml/018_prg_delta_delete.sql
@@ -2,6 +2,6 @@ delete from prg.delta_new prg
 where exists(
     select 1
     from osm_hashed osm
-    where st_dwithin(prg.geom, osm.geom, 5)
+    where st_dwithin(prg.geom, osm.geom, 2)
 );
 analyze prg.delta_new;
