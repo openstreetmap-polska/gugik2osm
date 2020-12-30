d5316e9db0e180e7ac9c6b0fb092420b79743b20
tomczk <tomasztaras@outlook.com>
2020-12-30 19:32:39 +0100

    fixed definition of bdot buildings types mappings

--- processing/sql/ddl/bdot_buildings_osm_tags_mapping.sql
+++ processing/sql/ddl/bdot_buildings_osm_tags_mapping.sql
@@ -7,6 +7,7 @@ create table if not exists bdot.buildings_categories_mappings (
   man_made text,
   leisure text,
   historic text,
+  tourism text,
   constraint buildings_categories_mappings_pk primary key (kategoria_bdot, funkcja_ogolna_budynku, funkcja_szczegolowa_budynku)
 );
 
