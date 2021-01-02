a3a8e6608b631e5631b36274860ae00d5884e9ed
tomczk <tomasztaras@outlook.com>
2021-01-02 13:06:13 +0100

    split queries switching prg.delta table into 3 files to be able to better monitor performance

--- processing/sql/dml/025a_prg_delta_switch_tables.sql
+++ processing/sql/dml/025a_prg_delta_switch_tables.sql
@@ -1,7 +1,8 @@
 alter table prg.delta rename to delta_old;
 alter table prg.delta_new rename to delta;
-drop table prg.delta_old;
+alter index prg.delta_gis rename to delta_gis_old;
+alter index prg.delta_lokalnyid rename to delta_lokalnyid_old;
+alter index prg.delta_simc rename to delta_simc_old;
 alter index prg.delta_gis_new rename to delta_gis;
 alter index prg.delta_lokalnyid_new rename to delta_lokalnyid;
 alter index prg.delta_simc_new rename to delta_simc;
-analyze prg.delta;
---
+++ processing/sql/dml/025c_prg_delta_drop_old.sql
@@ -0,0 +1,1 @@
+drop table prg.delta_old;
