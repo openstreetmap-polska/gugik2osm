e5693636cb69c705c701d84c7622ea57096df407
tomczk <tomasztaras@outlook.com>
2021-01-02 13:57:53 +0100

    added teryt street name column to street_names_mappings table

--- processing/sql/ddl/street_names_mappings.sql
+++ processing/sql/ddl/street_names_mappings.sql
@@ -1,6 +1,7 @@
 create table if not exists street_names_mappings (
   teryt_simc_code text not null,
   teryt_ulic_code text not null,
+  teryt_street_name text not null,
   osm_street_name text not null,
   constraint street_names_mappings_pk primary key (teryt_simc_code, teryt_ulic_code)
 );
