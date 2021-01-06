e9e63fc6c383523dc3d4c77d15a1f312cf6d3928
tomczk <tomasztaras@outlook.com>
2021-01-06 19:28:35 +0100

    changed housenumber standarization to be the same as for PRG addresses

--- processing/sql/dml/013_osm_adr_create.sql
+++ processing/sql/dml/013_osm_adr_create.sql
@@ -2,10 +2,26 @@ drop table if exists osm_adr;
 create table osm_adr as
 with
 tab as (
-    select miejscowosc, cz_msc, ulica, type as nr_porzadkowy, kod_pna, kod_ulic, kod_simc, ST_Centroid(geometry) geom
+    select
+      miejscowosc,
+      cz_msc,
+      ulica,
+      ltrim(rtrim(replace(replace(upper(trim(type)), '\', '/'), ' ', ''), './'), '.0/') as nr_porzadkowy,
+      kod_pna,
+      kod_ulic,
+      kod_simc,
+      ST_Centroid(geometry) geom
     from osm_addr_polygon
     union all
-    select miejscowosc, cz_msc, ulica, type as nr_porzadkowy, kod_pna, kod_ulic, kod_simc, geometry
+    select
+      miejscowosc,
+      cz_msc,
+      ulica,
+      ltrim(rtrim(replace(replace(upper(trim(type)), '\', '/'), ' ', ''), './'), '.0/') as nr_porzadkowy,
+      kod_pna,
+      kod_ulic,
+      kod_simc,
+      geometry
     from osm_addr_point
 ),
 a as (
@@ -13,7 +29,7 @@ a as (
       case when miejscowosc = '' then null else trim(miejscowosc) end msc,
       case when cz_msc = '' then null else trim(cz_msc) end czmsc,
       case when ulica = '' then null else trim(ulica) end ul,
-      upper(trim(nr_porzadkowy)) nr,
+      nr_porzadkowy as nr,
       case when kod_pna ~ '^\d{2}-\d{3}$' and kod_pna <> '00-000' then kod_pna else null end pna,
       case when kod_ulic ~ '^\d{5}$' then kod_ulic else null end ulic,
       case when kod_simc ~ '^\d{7}$' then kod_simc else null end simc,
@@ -22,9 +38,6 @@ a as (
 )
 SELECT
   a.*
---   ,st_srid(geom) srid,
---   st_x(geom) x,
---   st_y(geom) y
 FROM a
 where
 -- removed this condition as some addresses don't have either addr:city nor addr:place tags
