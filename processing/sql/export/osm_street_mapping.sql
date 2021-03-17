e12268c1e4447d9ff5b2653644bd3d949a1e1706
mskakuj <mskakuj@wp.pl>
2021-03-17 19:49:09 +0100

    correction of selection criteria

--- processing/sql/export/osm_street_mapping.sql
+++ processing/sql/export/osm_street_mapping.sql
@@ -1,7 +1,7 @@
 COPY (
 	select woj, pow, gmi, teryt_simc, teryt_msc, teryt_ulic, teryt_ulica
 	from prg.pa
-	where osm_ulica is null and lower(teryt_ulica) like '%. %'
+	where osm_ulica is null and lower(teryt_ulica) like '%.%'
 	group by woj, pow, gmi, teryt_simc, teryt_msc, teryt_ulic, teryt_ulica
 	order by teryt_ulica, teryt_msc
 ) TO STDOUT
