895005d335bd0c0963628138f622b839a5a2699e
tomczk <tomasztaras@outlook.com>
2021-04-18 20:35:40 +0200

    added scripts enabling/disabling site for maintenance

---
+++ conf/maintenance_mode_off.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+mv /opt/gugik2osm/web/index.html /opt/gugik2osm/web/maintenance.html
+mv /opt/gugik2osm/web/index_old.html /opt/gugik2osm/web/index.html
---
+++ conf/maintenance_mode_on.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+mv /opt/gugik2osm/web/index.html /opt/gugik2osm/web/index_old.html
+mv /opt/gugik2osm/web/maintenance.html /opt/gugik2osm/web/index.html
