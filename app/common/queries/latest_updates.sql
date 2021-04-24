c696730b4d89fc347a7c6dd964934bf3809854fe
tomczk <tomasztaras@outlook.com>
2021-04-24 19:22:42 +0200

    updated script to leverage PostGIS 3.0 functions

--- app/common/queries/latest_updates.sql
+++ app/common/queries/latest_updates.sql
@@ -2,28 +2,14 @@ with
 data as (
   select
     'osm' as dataset,
-    -- in postgis 3.0 there is a function to get bbox for xyz tile
-    -- ST_TileEnvelope(z, x, y) bbox,
-    ST_AsGeoJSON(ST_Transform(ST_MakeEnvelope(
-        -20037508.34 + (x * (20037508.34*2)/(2^z)),
-        20037508.34 - (y * (20037508.34*2)/(2^z)),
-        -20037508.34 + (x * (20037508.34*2)/(2^z)) + (20037508.34*2)/(2^z),
-        20037508.34 - (y * (20037508.34*2)/(2^z)) - (20037508.34*2)/(2^z),
-        3857
-    ), 4326))::jsonb bbox,
+    ST_AsGeoJSON(ST_Transform(ST_TileEnvelope(z, x, y), 4326))::jsonb bbox,
     to_char(created_at at time zone 'Europe/Warsaw', 'YYYY-MM-DD HH24:MI:SS') created_at
   from expired_tiles
   where 1=1
     and created_at is not null
     and created_at >= %(ts)s
     -- bbox is more or less in Poland
-    and ST_Transform(ST_MakeEnvelope(14.0, 49.0, 24.03, 54.86, 4326), 3857) && ST_MakeEnvelope(
-        -20037508.34 + (x * (20037508.34*2)/(2^z)),
-        20037508.34 - (y * (20037508.34*2)/(2^z)),
-        -20037508.34 + (x * (20037508.34*2)/(2^z)) + (20037508.34*2)/(2^z),
-        20037508.34 - (y * (20037508.34*2)/(2^z)) - (20037508.34*2)/(2^z),
-        3857
-    )
+    and ST_Transform(ST_MakeEnvelope(14.0, 49.0, 24.03, 54.86, 4326), 3857) && ST_TileEnvelope(z, x, y)
 
   union all
 
