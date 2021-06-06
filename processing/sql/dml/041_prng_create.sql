5516dc4ed4d4d5253eb260fc6afa60fa179712b9
tomczk <tomasztaras@outlook.com>
2021-06-06 13:58:52 +0200

    improved query generating table with locations for 'select random location' function, it will not count excluded (reported) objects from now on

--- processing/sql/dml/041_prng_create.sql
+++ processing/sql/dml/041_prng_create.sql
@@ -3,8 +3,24 @@ create table prng_new AS
 SELECT
     prng.id,
     prng.geom,
+    liczba_adresow,
+    liczba_budynkow,
     liczba_adresow+liczba_budynkow as count
 FROM stg_prng_miejscowosci prng
-LEFT JOIN lateral (select count(*) liczba_adresow from prg.delta a where st_dwithin(st_transform(prng.geom, 2180), a.geom, 1000)) adr on true
-LEFT JOIN lateral (select count(*) liczba_budynkow from bdot_buildings b where st_dwithin(prng.geom, b.geom_4326, 0.01)) bud on true
+LEFT JOIN lateral (
+    select count(*) liczba_adresow
+    from prg.delta a
+    left join exclude_prg ep on a.lokalnyid=ep.id
+    where 1=1
+        and st_dwithin(st_transform(prng.geom, 2180), a.geom, 1000)
+        and ep.id is null
+) adr on true
+LEFT JOIN lateral (
+    select count(*) liczba_budynkow
+    from bdot_buildings b
+    left join exclude_bdot_buildings ebb on b.lokalnyid=ebb.id
+    where 1=1
+        and st_dwithin(prng.geom, b.geom_4326, 0.01)
+        and ebb.id is null
+) bud on true
 ;
