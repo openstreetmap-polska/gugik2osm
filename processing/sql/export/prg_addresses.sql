28689804c07ec5f37ed0aab0d12b7bbb0da06c5f
tomczk <tomasztaras@outlook.com>
2021-08-05 21:20:19 +0200

    fixed query exporting addresses

--- processing/sql/export/prg_addresses.sql
+++ processing/sql/export/prg_addresses.sql
@@ -11,19 +11,16 @@ COPY (
         teryt_ulica ulica,
         nr,
         pna,
-        st_x(gml) x_2180,
-        st_y(gml) y_2180,
-        st_x(st_transform(gml, 4326)) x_4326,
-        st_y(st_transform(gml, 4326)) y_4326,
-        osm_ulica
-    from prg.pa prg
+        st_x(geom_2180) x_2180,
+        st_y(geom_2180) y_2180,
+        st_x(st_transform(geom_2180, 4326)) x_4326,
+        st_y(st_transform(geom_2180, 4326)) y_4326,
+        osm_street_name osm_ulica
+    from addresses prg
     join teryt.simc on prg.teryt_simc = simc.sym
     join teryt.terc on simc.woj=terc.woj and simc.pow=terc.pow and simc.gmi=terc.gmi and simc.rodz_gmi=terc.rodz
     join teryt.terc pow on simc.woj=pow.woj and simc.pow=pow.pow and pow.gmi is null
     join teryt.terc woj on simc.woj=woj.woj and woj.pow is null
-    where
-        prg.teryt_msc is not null
-        and not (prg.teryt_ulic is null and prg.ul is not null)
-        and not (simc.rm like '9%' and prg.teryt_ulica is null)
+    left join street_names_mappings sm on teryt_simc_code=prg.teryt_simc and teryt_ulic_code=prg.teryt_ulic
 ) TO STDOUT
 WITH CSV HEADER ENCODING 'UTF-8'
