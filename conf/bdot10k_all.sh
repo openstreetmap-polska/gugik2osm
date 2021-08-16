ba275683a87e5342b2df2f26104187f22c47406d
tomczk <tomasztaras@outlook.com>
2021-08-16 21:17:53 +0200

    moved bash scripts running data processing jobs to more suitable location

--- conf/bdot10k_all.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# exit on error in any command
-set -e
-
-/opt/gugik2osm/conf/bdot10k_dl.sh
-/opt/gugik2osm/conf/bdot10k_bubd_a_parse.sh
-/opt/gugik2osm/conf/bdot10k_bubd_a_export.sh
---
+++ processing/bash/bdot10k_all.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# exit on error in any command
+set -e
+
+/opt/gugik2osm/git/processing/bdot10k_dl.sh
+/opt/gugik2osm/git/processing/bdot10k_bubd_a_parse.sh
+/opt/gugik2osm/git/processing/bdot10k_bubd_a_export.sh
