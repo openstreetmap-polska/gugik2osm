c18c5386e7b89a7b2ca3bc5f43988e476c9d0157
tomczk <tomasztaras@outlook.com>
2021-08-22 16:02:29 +0200

    updated mappings of teryt street types

--- processing/sql/ddl/teryt_cecha_mapping.sql
+++ processing/sql/ddl/teryt_cecha_mapping.sql
@@ -6,18 +6,18 @@ create table if not exists teryt.cecha_mapping (
 -- m column is going to be either text mapping or empty string for further ease of processing
 insert into teryt.cecha_mapping values
 ('ul.', ''),
-('al.', 'aleja'),
-('pl.', 'plac'),
-('skwer', 'skwer'),
-('bulw.', 'bulwar'),
-('rondo', 'rondo'),
-('park', 'park'),
-('rynek', 'rynek'),
+('al.', 'Aleja'),
+('pl.', 'Plac'),
+('skwer', 'Skwer'),
+('bulw.', 'Bulwar'),
+('rondo', 'Rondo'),
+('park', 'Park'),
+('rynek', 'Rynek'),
 ('szosa', ''),
 ('droga', ''),
-('os.', 'osiedle'),
-('wyspa', 'wyspa'),
-('wyb.', 'wybrzeże'),
+('os.', 'Osiedle'),
+('wyspa', 'Wyspa'),
+('wyb.', 'Wybrzeże'),
 ('inne', '')
 on conflict do nothing
 ;
