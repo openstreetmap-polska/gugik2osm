73abc329969e9e36d8cb78e9f7de8f976d16fd41
tomczk <tomasztaras@outlook.com>
2021-09-19 14:04:51 +0200

    refactored some SQL queries

--- processing/sql/ddl/street_names_mappings.sql
+++
@@ -1,9 +0,0 @@
-drop table if exists street_names_mappings;
-
-create table street_names_mappings (
-  teryt_simc_code text not null,
-  teryt_ulic_code text not null,
-  teryt_street_name text not null,
-  osm_street_name text not null,
-  constraint street_names_mappings_pk primary key (teryt_simc_code, teryt_ulic_code)
-);
