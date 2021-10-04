c8561f3d286c5398274c4a697fd16cc99d76c4ba
tomczk <tomasztaras@outlook.com>
2021-10-04 21:27:09 +0200

    fixed script deploy script after changes to ddl files structure

--- conf/deploy_street_names_mappings.sh
+++ conf/deploy_street_names_mappings.sh
@@ -13,7 +13,7 @@ set +a
 date >> /opt/gugik2osm/log/prg_processing.log
 echo "Deploying street names mappings..." >> /opt/gugik2osm/log/prg_processing.log
 
-psql -h $PGHOSTADDR -p $PGPORT -d $PGDATABASE -U $PGUSER -f /opt/gugik2osm/git/processing/sql/ddl/street_names_mappings.sql >> /opt/gugik2osm/log/prg_processing.log 2>&1
+psql -h $PGHOSTADDR -p $PGPORT -d $PGDATABASE -U $PGUSER -c "truncate table street_names_mappings" >> /opt/gugik2osm/log/prg_processing.log 2>&1
 cat /opt/gugik2osm/git/processing/sql/data/street_names_mappings.csv | psql -h $PGHOSTADDR -p $PGPORT -d $PGDATABASE -U $PGUSER -c "copy street_names_mappings FROM stdin with CSV header delimiter ','" >> /opt/gugik2osm/log/prg_processing.log 2>&1
 
 echo "Done." >> /opt/gugik2osm/log/prg_processing.log
