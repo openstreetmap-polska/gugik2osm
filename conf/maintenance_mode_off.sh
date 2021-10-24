a105a5c9d0c6009831dd341ef693c3246352d7e4
tomczk <tomasztaras@outlook.com>
2021-10-24 17:26:51 +0200

    added db lock set up to maintenance scripts

--- conf/maintenance_mode_off.sh
+++ conf/maintenance_mode_off.sh
@@ -1,4 +1,29 @@
 #!/bin/bash
 
+# exit on error in any command
+set -e
+
 mv /opt/gugik2osm/web/index.html /opt/gugik2osm/web/maintenance.html
 mv /opt/gugik2osm/web/index_old.html /opt/gugik2osm/web/index.html
+
+set -a
+source /opt/gugik2osm/conf/.env
+set +a
+
+query=$(cat << EOF
+UPDATE process_locks
+SET (
+        in_progress,
+        end_time,
+        last_status
+    ) = (
+        false,
+        CURRENT_TIMESTAMP,
+        'SUCCESS'
+    )
+WHERE process_name = 'db_lock'
+;
+EOF
+)
+
+psql -h $PGHOSTADDR -p $PGPORT -d $PGDATABASE -U $PGUSER -c "$query"
