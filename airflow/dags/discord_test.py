f7e2767be0c2598be3e0863e517ef38f60d0028f
tomasz t <tomasztaras@outlook.com>
2022-03-28 00:37:23 +0200

    added airflow test dag

---
+++ airflow/dags/discord_test.py
@@ -0,0 +1,32 @@
+import datetime
+
+from airflow import DAG
+from airflow.models.baseoperator import chain
+from airflow.providers.discord.operators.discord_webhook import DiscordWebhookOperator
+from airflow.providers.postgres.operators.postgres import PostgresOperator
+
+
+with DAG(
+    dag_id="test_postgresql_and_discord",
+    description="Test if we can connect to PostgreSQL database and send Discord message.",
+    start_date=datetime.datetime(2022, 3, 27),
+    schedule_interval="@once",
+    catchup=False,
+):
+    pg_version_task_id = "postgresql_version"
+    pg_check_version_task = PostgresOperator(
+        task_id=pg_version_task_id,
+        sql="SELECT version();",
+        do_xcom_push=True,
+    )
+
+    xcom_template = "{{ task_instance.xcom_pull(task_ids='" + pg_version_task_id + "') }}"
+    send_discord_message_task = DiscordWebhookOperator(
+        task_id="send_discord_message",
+        message="Testing connectivity. PostgreSQL version: " + xcom_template,
+    )
+
+    chain(
+        pg_check_version_task,
+        send_discord_message_task,
+    )
