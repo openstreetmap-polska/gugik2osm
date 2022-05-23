bdb02d0d571433174e06a79daf8998c307805092
tomasz t <tomasztaras@outlook.com>
2022-05-24 00:44:52 +0200

    fixed bash commands

--- airflow/dags/files_cleanup_dag.py
+++ airflow/dags/files_cleanup_dag.py
@@ -21,7 +21,7 @@ with DAG(
         task_id="remove_old_imposm_files",
         bash_command=r"""
             set -e;
-            find /opt/gugik2osm/imposm3/imposm_diff/0* -mtime +7 -type f -exec rm {} \;
+            find /opt/gugik2osm/imposm3/imposm_diff/0* -mtime +7 -type f -print -delete;
             find /opt/gugik2osm/imposm3/imposm_diff/0* -empty -type d -delete;
         """,
     )
@@ -30,7 +30,7 @@ with DAG(
         task_id="remove_old_changesets",
         bash_command=r"""
             set -e;
-            find /opt/gugik2osm/temp_changesets/**/*.osm.gz -mtime +1 -type f -exec rm {} \;
+            find /opt/gugik2osm/temp_changesets/ -name "*.osm.gz" -mtime +1 -type f -print -delete;
             find /opt/gugik2osm/temp_changesets/ -empty -type d -delete;
         """,
     )
@@ -48,7 +48,7 @@ with DAG(
         task_id="remove_old_airflow_logs",
         bash_command=r"""
             set -e;
-            find /opt/gugik2osm/airflow/logs/**/*.log -mtime +15 -type f -exec rm {} \;
+            find /opt/gugik2osm/airflow/logs/**/*.log -mtime +15 -type f -print -delete;
             find /opt/gugik2osm/airflow/logs/ -empty -type d -delete;
         """,
     )
