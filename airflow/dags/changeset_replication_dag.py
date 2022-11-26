bd94f48b3554ac6e39fff8ee49c1c48aeaebf895
tomasz t <tomasztaras@outlook.com>
2022-11-26 14:40:40 +0100

    changed storage place to s3

--- airflow/dags/changeset_replication_dag.py
+++ airflow/dags/changeset_replication_dag.py
@@ -2,25 +2,26 @@ import datetime
 import logging
 from functools import partial
 from pathlib import Path
+from tempfile import TemporaryDirectory
 
+import pyarrow as pa
+import pyarrow.parquet as pq
 from airflow import DAG, AirflowException
 from airflow.models import Variable, TaskInstance
 from airflow.models.baseoperator import chain
 from airflow.operators.python import PythonOperator, ShortCircuitOperator
-import pyarrow as pa
-import pyarrow.parquet as pq
+from airflow.providers.amazon.aws.hooks.s3 import S3Hook
 
+from utils.discord import send_dag_run_status
 from utils.osm.changesets import find_newest_changeset_replication_sequence, changesets_between_sequences
 from utils.parquet import changeset_schema
-from utils.discord import send_dag_run_status
-
 
 logger = logging.getLogger()
 
 send_dag_run_status_to_discord = partial(send_dag_run_status, antispam=False)
 
 last_processed_sequence_variable_name = "last_processed_changeset_sequence"
-date_base_dir = Path("/var/www/data/changesets/replication/")
+s3_bucket_name = "s3://tt-osm-changesets/"
 
 
 def check_if_anything_to_run(**kwargs) -> bool:
@@ -41,12 +42,26 @@ def process_changesets(**kwargs) -> None:
     logger.info(f"Converted changesets to PyArrow Table with: {table.num_rows} rows.")
     ti: TaskInstance = kwargs["ti"]
     processed_datetime = ti.start_date
-    relative_path = f"{processed_datetime.date().isoformat()}/{processed_datetime.isoformat()}.parquet"
-    logger.info(f"Saving to file name: {relative_path}")
-    file_path = date_base_dir / relative_path
-    file_path.parent.mkdir(exist_ok=True)  # make sure dir exists
-    pq.write_table(table, file_path.as_posix())
-    logger.info("File saved successfully.")
+    date_str = processed_datetime.date().isoformat()
+    ts_str = processed_datetime.isoformat()
+    with TemporaryDirectory() as temp_dir_name:
+        temp_dir = Path(temp_dir_name)
+        parquet_file_path = temp_dir / date_str / f"{ts_str}.parquet"
+        pq.write_table(table, parquet_file_path.as_posix())
+        logger.info("Temp file saved successfully.")
+        hook = S3Hook(aws_conn_id="aws_tt_s3")
+        hook.load_file(
+            filename=parquet_file_path,
+            bucket_name=s3_bucket_name,
+            key=f"replication/dag_run_date={date_str}/{ts_str}.parquet",
+        )
+        hook.load_string(
+            string_data=newest.formatted,
+            bucket_name=s3_bucket_name,
+            key="state.txt",
+            replace=True,
+        )
+        logger.info("Files uploaded to s3 successfully.")
     Variable.set(last_processed_sequence_variable_name, newest.formatted)
     logger.info(f"Variable: {last_processed_sequence_variable_name} set to: {newest.formatted}")
 
