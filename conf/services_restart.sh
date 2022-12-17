7972bd3c0a6d9f48870f28b9cf0e1c6069a77259
tomasz t <tomasztaras@outlook.com>
2022-12-17 18:55:04 +0100

    fixed typo

--- conf/services_restart.sh
+++ conf/services_restart.sh
@@ -3,12 +3,12 @@
 service imposm stop
 service supervisor stop
 service airflow-webserver stop
-service airflow-scheudler stop
+service airflow-scheduler stop
 service pgbouncer stop
 service postgresql stop
 service postgresql start
 service pgbouncer start
 service supervisor start
-service airflow-scheudler start
+service airflow-scheduler start
 service airflow-webserver start
 service imposm start
