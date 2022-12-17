a1d1acf84913ca2c747bb1d33699dfe72dfa90c2
tomasz t <tomasztaras@outlook.com>
2022-12-17 18:53:30 +0100

    added pgbouncer and airflow to list of restarted services

--- conf/services_restart.sh
+++ conf/services_restart.sh
@@ -2,7 +2,13 @@
 
 service imposm stop
 service supervisor stop
+service airflow-webserver stop
+service airflow-scheudler stop
+service pgbouncer stop
 service postgresql stop
 service postgresql start
+service pgbouncer start
 service supervisor start
+service airflow-scheudler start
+service airflow-webserver start
 service imposm start
