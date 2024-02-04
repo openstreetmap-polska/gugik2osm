9dd8471dea9cbf80f67c052e7e9a98cd04765773
polish96 <91760516+polish96@users.noreply.github.com>
2024-02-04 08:04:34 +0100

    Delete 025_prg_delta_delete.sql

--- processing/sql/dml/025_prg_delta_delete.sql
+++
@@ -1,9 +0,0 @@
--- set jit=on;
-
--- delete from prg.delta_new prg
--- where exists(
---     select 1
---     from osm_hashed osm
---     where st_dwithin(prg.geom, osm.geom, 2)
--- );
--- analyze prg.delta_new;
