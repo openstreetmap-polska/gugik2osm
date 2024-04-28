92769228915958c4969d885fb9ac255838f871d4
Kamil Monicz <kamil@monicz.dev>
2024-04-28 15:51:15 +0200

    sudo preserve env path

--- conf/deploy.sh
+++ conf/deploy.sh
@@ -31,7 +31,7 @@ chmod 600 /etc/systemd/system/create_socket_folder.service
 
 systemctl daemon-reload
 
-sudo --preserve-env -u ttaras python3 git/processing/scripts/overpass2geojson.py \
+sudo --preserve-env=PATH -u ttaras python3 git/processing/scripts/overpass2geojson.py \
     --input-dir git/processing/overpass \
     --output-dir /var/www/overpass-layers \
     --skip-existing
