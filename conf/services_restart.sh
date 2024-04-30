7f764d65603ffca461f594a6a103978e46871cd9
Kamil Monicz <kamil@monicz.dev>
2024-04-30 21:30:40 +0200

    Remove pgbouncer

--- conf/services_restart.sh
+++ conf/services_restart.sh
@@ -4,11 +4,9 @@ systemctl stop imposm
 systemctl stop supervisor
 systemctl stop airflow-webserver
 systemctl stop airflow-scheduler
-systemctl stop pgbouncer
 systemctl stop postgresql
 
 systemctl start postgresql
-systemctl start pgbouncer
 systemctl start supervisor
 systemctl start airflow-scheduler
 systemctl start airflow-webserver
