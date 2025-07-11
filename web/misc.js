6d159da3d71dd164a55c5579c94dad0770433b9a
robot8a <cie.hochoa@gmail.com>
2025-07-11 22:28:15 +0200

    Add basic i18n functionality

--- web/misc.js
+++ web/misc.js
@@ -28,3 +28,30 @@ function processesInfo () {
             $('#procinfo').html(temp);
     });
 }
+
+i18next
+.use(i18nextHttpBackend)
+.init({
+    lng: 'pl', // default language
+    fallbackLng: 'pl',
+    debug: true,
+    backend: {
+        loadPath: './locales/{{lng}}.json'
+    }
+}, function(err, t) {
+    updateContent();
+});
+
+function updateContent() {
+    document.querySelectorAll('[i18n]').forEach(el => {
+        const key = el.getAttribute('i18n');
+        el.innerHTML = i18next.t(key);
+    });
+}
+
+function updateLanguage(lang) {
+    i18next.changeLanguage(lang, (err, t) => {
+        if (err) return console.error(err);
+        updateContent();
+    });
+}
