603dc43b8ef9ae7057f7d11220651a6c0671aef8
robot8a <cie.hochoa@gmail.com>
2025-07-11 23:46:13 +0200

    Add status modal and processes translation

--- web/misc.js
+++ web/misc.js
@@ -7,25 +7,26 @@ function processesInfo () {
             console.log(data);
             var temp = '<table>';
             data.processes.forEach((el) => {
-                temp += `<tr><td><b>Nazwa:</b></td><td><b>${el.name}</b></td>`;
+                temp += `<tr><td><b i18n="processes.name"></b></td><td><b>${el.name}</b></td>`;
                 if (el.in_progress) {
-                    temp += `<tr><td>Czy w trakcie:</td><td style="background-color: #66ff33;">Tak</td>`;
+                    temp += `<tr><td i18n="processes.in-progress"></td><td style="background-color: #66ff33;" i18n="processes.yes"></td>`;
                 } else {
-                    temp += `<tr><td>Czy w trakcie:</td><td>Nie</td>`;
+                    temp += `<tr><td i18n="processes.in-progress"></td><td i18n="processes.yes"></td>`;
                 }
-                temp += `<tr><td>Czas rozpoczęcia:</td><td>${el.start_time}</td>`;
+                temp += `<tr><td i18n="processes.start-time"></td><td>${el.start_time}</td>`;
                 if (el.end_time != null) {
-                    temp += `<tr><td>Czas zakończenia:</td><td>${el.end_time}</td>`;
+                    temp += `<tr><td i18n="processes.end-time"></td><td>${el.end_time}</td>`;
                 }
                 if (el.no_of_tiles_to_process != null && el.no_of_tiles_to_process >= 0) {
-                    temp += `<tr><td>Kafle pozostałe do przetworzenia:</td><td>${el.no_of_tiles_to_process}</td>`;
+                    temp += `<tr><td i18n="processes.no-of-tiles-to-process"></td><td>${el.no_of_tiles_to_process}</td>`;
                 }
                 if (el.last_status != null) {
-                    temp += `<tr><td>Status ostatniego wykonania:</td><td>${el.last_status}</td>`;
+                    temp += `<tr><td i18n="processes.last-status"></td><td>${el.last_status}</td>`;
                 }
             });
             temp += '</table>';
             $('#procinfo').html(temp);
+            updateContent();
     });
 }
 
@@ -56,6 +57,8 @@ function updateContent() {
 function updateLanguage(lang) {
     i18next.changeLanguage(lang, (err, t) => {
         if (err) return console.error(err);
+        document.documentElement.lang = lang;
+        document.querySelector('html').setAttribute('lang', lang);
         updateContent();
     });
 }
